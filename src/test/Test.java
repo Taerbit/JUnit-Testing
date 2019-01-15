94ed0092374557df7e2a0ff1153c2a52602ff492
finntorbet <finntorbet@labpc053.computing.dundee.ac.uk>
2019-01-15 14:50:16 +0000

    Initial Commit

---
+++ src/test/Test.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author finntorbet
+ */
+public class Test {
+    
+    public Test() {
+  
+    }
+    
+    public void print() {
+        String s = "test";
+        for(int i = 0; i < 5; i++) {
+            System.out.print(s + " " + i + "\n");
+        }
+    }
+}
